7b57535d781969fe0ba4e999e14c1e40eea03f42
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-07-25 11:30:00 +0600

    test: add integration tests for shutdown module using tokio::test

---
+++ UserDefinedTyoe/array-types.ts
@@ -0,0 +1,10 @@
+let UserName: Array<string>;
+
+UserName = ["anis" , "Atik"];
+console.log(UserName[0]);
+
+let multipletypes: string|number[];
+multipletypes = [1 , "name"];
+
+UserName.sort();
+console.log(UserName);
---
+++ build-in-Type/index.ts
@@ -0,0 +1,12 @@
+let userId: number;
+let UserName: string;
+let isActivated: boolean;
+let lastName: string;
+
+userId = 101;
+UserName = "habid";
+lastName = "Khan";
+isActivated = true;
+
+console.log (userId);
+console.log (`Your id : ${userId}` , `UserName: ${UserName} `);
