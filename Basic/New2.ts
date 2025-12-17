485e079a02b02b0e4999b82f790cf68ca53781e0
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-17 23:08:39 +0600

    Hello

---
+++ Basic/New2.ts
@@ -0,0 +1,16 @@
+export class Person {
+  name: string;
+  age: number;
+
+  constructor(name: string, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  getDetails(): string {
+    return ` 'Name: ${this.name}, Age: ${this.age } '`;
+  }
+}
+
+
+// ja ache kopal a 
