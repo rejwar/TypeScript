d3f3fe6a4e50103222dd2ca6281ed569cd99ee1b
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 03:39:02 +0600

    Basic Primitive

---
+++ Basic/PrimitiveTypes.rs
@@ -0,0 +1,16 @@
+
+let user: undefined;
+
+let userRole: null;
+
+userRole = null;
+
+console.log(userRole=== user);
+
+if (!userRole){
+    console.log("The condition is true");
+} 
+
+if (!user) {
+    console.log("This condition is true");
+}
