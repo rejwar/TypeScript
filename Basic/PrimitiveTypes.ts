85a8aab96256a1872aef5132adb6fb35dd3dc7ba
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 04:10:04 +0600

    Biut

---
+++ Basic/PrimitiveTypes.ts
@@ -0,0 +1,7 @@
+let message:string = "Hello  , typeScript";
+let age: number = 42;
+let isStudent: boolean = true;
+let fetched: null = null;
+let user: undefined= undefined;
+let leargenumber: bigint = 9024352452345n;
+let unique: symbol = Symbol("uniqueSymbol");
