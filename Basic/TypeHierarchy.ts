cfe8111145a50c649b9a3659a4fa99f00ae96921
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 14:23:25 +0600

    Hello World

--- Basic/TypeHierarchy.ts
+++ Basic/TypeHierarchy.ts
@@ -2,3 +2,11 @@ type check = any extends unknown ? true : false;
 type  check2 = any extends unknown ? true : false;
 type check3 = null extends any ? true : false;
 type check4 = undefined extends void ? true: false;
+type check5 = [] extends Object ? true : false;
+type check6 = function throwError(erroMessage:String) => {
+    throw new Error(errMessage)
+}
+
+
+
+let strings: Object = ["a", "b"];
