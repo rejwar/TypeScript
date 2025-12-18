776b76da0a0797e5ec9464b04990964a0cf05ee6
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 12:38:12 +0600

    Alias

---
+++ Basic/TypeAlies.ts
@@ -0,0 +1,9 @@
+type CustomString = string;
+type CustomNumber = number;
+type CustomData = Data;
+type CustomSymbol = Symbol;
+
+let firstName: CustomString = "Mark";
+let age: CustomNumber = 32;
+let today: CustomData = new Data();
+let unique: CustomSymbol = symbol();
