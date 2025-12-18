a67a3b0e14a72086bb587085a3a92d9e960b6a57
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 14:13:07 +0600

    Hello

---
+++ Basic/TypeHierarchy.ts
@@ -0,0 +1,4 @@
+type check = any extends unknown ? true : false;
+type  check2 = any extends unknown ? true : false;
+type check3 = null extends any ? true : false;
+type check4 = undefined extends void ? true: false;
