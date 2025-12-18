a5469d935c2a99919e21c9a7f8a39359f1d77b98
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 14:51:25 +0600

    nbew car

---
+++ Basic/Car.ts
@@ -0,0 +1,22 @@
+let person {
+    name: " Mark",
+    age: 32
+}
+
+let car = {
+    brand: "BMW",
+    color: "Black",
+};
+
+
+
+car [];
+car = () => {};
+
+ler newCar: {} = {
+    
+    brand: "BMW",
+    color: "Black",
+};
+
+newCar = [];
