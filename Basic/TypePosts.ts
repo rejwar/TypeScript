729dfd42012a77e8369e576245e033fab7b74e6b
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 14:57:02 +0600

    hello

---
+++ Basic/TypePosts.ts
@@ -0,0 +1,9 @@
+let post : {
+    title : "This is a blog post",
+    content: "Content of the post",
+    date: new DataTransfer(),
+} = {
+    title: "This is a blog post ",
+    content: "Content of the post ",
+    date: new Date(),
+};
