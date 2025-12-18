afb4ca2c8661b1eec398285b71b875d34343167f
Md. Rejwar Rahman Rifat <mdrejwarrahman1010@gmail.com>
2025-12-18 13:38:30 +0600

    Hello shifa

--- Basic/TypeAlies.ts
+++ Basic/TypeAlies.ts
@@ -7,3 +7,12 @@ let firstName: CustomString = "Mark";
 let age: CustomNumber = 32;
 let today: CustomData = new Data();
 let unique: CustomSymbol = symbol();
+
+
+
+function addNumbers(a: number , b: number) {
+    return a+b;
+}
+
+
+let finalResult = addNumbers(10,20);
